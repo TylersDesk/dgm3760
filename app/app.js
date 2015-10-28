b3ba720075d1563f006c7b0086c6393097bf7205
TylersDesk <tymayn@gmail.com>
2015-10-28 17:27:46 -0600

    Base work for students

--- app/app.js
+++ app/app.js
@@ -4,14 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var addstudent = require('./routes/addstudent');
-
-
-mongoose.connect('mongodb://localhost/uvuclass');
 
 var app = express();
 
@@ -28,8 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/addstudent', addstudent)
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
